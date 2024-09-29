cff7aacf5082e784244f73700a42525eeb35f085
carloscordova.dev <cordovamanuel22@icloud.com>
2024-09-29 08:53:35 -0700

    feat: :sparkles: add knew init

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      port: process.env.SUPA_DB_PORT_PROC,
+      host: process.env.SUPA_DB_HOST_PROC ,
+      database: process.env.SUPA_DB_NAME_PROC,
+      user: process.env.SUPA_DB_USERNAME_PROC,
+      password: process.env.SUPA_BD_PASSWORD_PROC
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
