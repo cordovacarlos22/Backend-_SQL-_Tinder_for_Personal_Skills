274377353f0659f9aee36eb3f703b97bafeaf380
carloscordova.dev <cordovamanuel22@icloud.com>
2024-09-29 09:10:24 -0700

    feat(server): :sparkles: config server

---
+++ index.js
@@ -0,0 +1,28 @@
+// ? this imports dotenv to be able to use environment variables
+require('dotenv').config()
+
+
+// ! #1 imports express
+const express = require('express')
+// ! #2 imports create an instance of express
+const app = express()
+
+// ! #3 sets up the port for the server
+const port = process.env.SERVER_PORT || 3000
+
+// ! #4 sets up middleware to parse JSON requests
+app.use(express.json())
+
+// ! #5 sets up the endpoint for testing purposes
+app.get('/test', (req, res) => {
+  try {
+    res.status(200).json("success")
+  } catch (error) {
+    res.status(500).json("error")
+  }
+});
+
+// ! #6 starts the server and listens on the specified port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+});
